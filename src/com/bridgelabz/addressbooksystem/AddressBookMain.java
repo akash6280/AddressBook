a3dba0ec298106e0a637b6b7d4a4d08337d9aff9
Akash Singh <akashsingh6280@gmail.com>
2021-08-31 02:08:27 +0530

    ADDED: Using console to add details of person

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -5,6 +5,7 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Address Book Program");
-		
+		AddressBookFacilitiesImpl services=new AddressBookFacilitiesImpl();
+		services.createContact();
 	}
 }
