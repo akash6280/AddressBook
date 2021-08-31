7375f8a6c9af6d22dc81830930faa0a45906d4fd
Akash Singh <akashsingh6280@gmail.com>
2021-08-31 05:36:16 +0530

    ADDED: To create address book with unique name

--- src/com/bridgelabz/addressbooksystem/AddressBookFacilitiesImpl.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookFacilitiesImpl.java
@@ -4,6 +4,43 @@ public class AddressBookFacilitiesImpl implements AddressBookFacilitiesIF {
 	
 	Scanner scanner=new Scanner(System.in);
 	AddressBook addressbook;
+	public void createAddressBook() {
+		
+		System.out.println("Enter book name");
+		String bookName=scanner.nextLine();
+		
+		ContactPerson[] contactList;
+		System.out.println("Enter number of contact you want to create");
+		int numberOfContact=scanner.nextInt();
+		contactList=new ContactPerson[numberOfContact];
+		scanner.nextLine();
+		
+		for(int index=0;index<numberOfContact;index++) {
+			
+			System.out.println("Enter first name: ");
+			String firstName = scanner.nextLine();
+			System.out.println("Enter last name: ");
+			String lastName = scanner.nextLine();
+			System.out.println("Enter address: ");
+			String address = scanner.nextLine();
+			System.out.println("Enter city: ");
+			String city = scanner.nextLine();
+			System.out.println("Enter state: ");
+			String state = scanner.nextLine();
+			System.out.println("Enter Zip Code: ");
+			int zip = scanner.nextInt();
+			System.out.println("Enter Phone Number: ");
+			long phoneNumber = scanner.nextLong();
+			scanner.nextLine();
+			System.out.println("Enter email id: ");
+			String email = scanner.nextLine();
+			
+			contactList[index]=new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		}
+		    
+			addressbook=new AddressBook(bookName,contactList);
+	}
+	
 	public  void createContacts() {
 		
 		ContactPerson[] contactList;
@@ -35,8 +72,9 @@ public class AddressBookFacilitiesImpl implements AddressBookFacilitiesIF {
 			contactList[index]=new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
 		}
 		    
-			addressbook=new AddressBook(contactList);
+			addressbook=new AddressBook("first",contactList);
 	}
+	
 	public void editContacts() {
 			System.out.println("Enter phone number of person");
 			Long phoneNumber=scanner.nextLong();
