30d029fc20f14dc8319da200dcc2f7465a10298a
Akash Singh <akashsingh6280@gmail.com>
2021-09-14 01:32:56 +0530

    ADDED: To abtract modify address book method

--- src/com/bridgelabz/addressbooksystem/AddressBookFacilitiesIF.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookFacilitiesIF.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.addressbooksystem;
 
 public interface AddressBookFacilitiesIF {
-	public void createContacts();
+	public ContactPerson createContacts();
 	public void editContacts();
 	public void deleteContacts();
 	public void createAddressBook();
+	public void modifyAddressBook();
 }
