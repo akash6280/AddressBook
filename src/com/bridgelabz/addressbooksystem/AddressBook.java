8b23faba5dc13a355025a0b14e91deae6824f88b
Akash Singh <akashsingh6280@gmail.com>
2021-09-14 00:46:42 +0530

    REFACTORED: replaced linked list with array

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -1,21 +1,34 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.LinkedList;
+
 public class AddressBook {
-	    String name;
-		private  ContactPerson[] contactList;
+	    private String bookName;
+		private LinkedList<ContactPerson> contactList;
 		
-	    AddressBook(String name,ContactPerson[] contactList) {
-			this.contactList=contactList;
-			this.name=name;
-		}
-
-		public ContactPerson[] getContactList() {
-			return contactList;
-		}
-
-		public void setContactList(ContactPerson[] contactList) {
+		public AddressBook(String bookName, LinkedList<ContactPerson> contactList) {
+			this.bookName = bookName;
 			this.contactList = contactList;
 		}
 
+		public String getBookName() {
+			return bookName;
+		}
+
+		public void setBookName(String bookName) {
+			this.bookName = bookName;
+		}
+
+		public void setContactList(LinkedList<ContactPerson> contactList) {
+			this.contactList = contactList;
+		}
+
+		public LinkedList<ContactPerson> getContactList() {
+			return contactList;
+		}
+		@Override
+		public String toString() {
+			return "\nAddressBook [bookName=" + bookName + ", contactList=" + contactList + "]";
+		}
 	    
 }
