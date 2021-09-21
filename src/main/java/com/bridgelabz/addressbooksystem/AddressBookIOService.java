b8a6c03a3e3332fc042e5566192c8344b5692cb0
Akash Singh <akashsingh6280@gmail.com>
2021-09-22 02:41:11 +0530

    ADDED:  method to write data to csv file

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookIOService.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookIOService.java
@@ -2,24 +2,36 @@ package com.bridgelabz.addressbooksystem;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
 
 public class AddressBookIOService {
 
-    public static String ADDRESSBOOK_FILE_NAME = "addressbook-file.txt";
+    public static String ADDRESSBOOK_FILE_NAME_TXT = "addressbook-file.txt";
+    public static String ADDRESSBOOK_FILE_NAME_CSV = "addressbook-file.csv";
 
-    public void writeData(List<ContactPerson> employeePayrollList) {
+    public void writeDataToTextFile(List<ContactPerson> contactList) {
         StringBuffer empBuffer = new StringBuffer();
-        employeePayrollList.forEach(employee -> {
+        contactList.forEach(employee -> {
             String employeeDataString = employee.toString().concat("\n");
             empBuffer.append(employeeDataString);
         });
 
         try {
-            Files.write(Paths.get(ADDRESSBOOK_FILE_NAME), empBuffer.toString().getBytes());
+            Files.write(Paths.get(ADDRESSBOOK_FILE_NAME_TXT), empBuffer.toString().getBytes());
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -28,18 +40,16 @@ public class AddressBookIOService {
         
     public void printData() {
     		try {
-    			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath()).forEach(System.out::println);
+    			Files.lines(new File(ADDRESSBOOK_FILE_NAME_TXT).toPath()).forEach(System.out::println);
     		} catch(IOException e) {
     			e.printStackTrace(); 
     		}
     	}
 	
-	public void readDataFromFile() {
-			
-			List<String> addressBookList = new ArrayList<String>();
+	public void readDataFromTextFile() {
 			
 			try {
-				Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
+				Files.lines(new File(ADDRESSBOOK_FILE_NAME_TXT).toPath())
 					.map(line -> line.trim())
 					.forEach(System.out::println);
 				
@@ -48,5 +58,22 @@ public class AddressBookIOService {
 				e.printStackTrace();
 			}
 		}
+	
+	 public void writeDataToCsvFile(List<ContactPerson> contactList) {
+		 try(
+				 Writer writer =Files.newBufferedWriter(Paths.get(ADDRESSBOOK_FILE_NAME_CSV))
+			){
+				StatefulBeanToCsv<ContactPerson> beanToCsv= new StatefulBeanToCsvBuilder<ContactPerson>(writer)
+															.withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+															.build();
+
+				beanToCsv.write(contactList);
+			}
+		 catch (IOException | CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
+	            e.printStackTrace();
+	        }
+		 
+		 
+	 }
 
 }
