7e149e7f2ed7c292851953a985e37e6ee2d876f7
Akash Singh <akashsingh6280@gmail.com>
2021-09-30 06:48:00 +0530

    ADDED: test case to verify employee count between a date range

--- src/test/java/com/bridgelabz/addressbooksystem/AddressBookDBIOTest.java
+++ src/test/java/com/bridgelabz/addressbooksystem/AddressBookDBIOTest.java
@@ -28,4 +28,14 @@ public class AddressBookDBIOTest {
 		boolean result = addressBookService.checkContactInSyncWithDB("yash");
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void givenDateRange_WhenQueried_ShouldReturnContactCount(){
+		AddressBookFacilitiesImpl addressBookService=new AddressBookFacilitiesImpl();
+		addressBookService.readContactData(IOService.DB_IO);
+		LocalDate startDate = LocalDate.of(2019,01,01);
+		LocalDate endDate = LocalDate.now(); 
+		List<ContactPerson> contactList = addressBookService.getContactsInADateRange(startDate,endDate);
+		Assert.assertEquals(6, contactList.size());
+	}
 }
