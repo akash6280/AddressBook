f903a8904e7baf96f3753103ffc4e77ecca4bf8d
Akash Singh <akashsingh6280@gmail.com>
2021-09-30 06:05:39 +0530

    ADDED: method to update detail in database

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookDBService.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookDBService.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbooksystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,6 +12,14 @@ import java.util.List;
 
 
 public class AddressBookDBService {
+	private PreparedStatement addressBookDataStatement;
+	private static AddressBookDBService addressBookDBService;
+	
+	public static AddressBookDBService getInstance(){
+		if (addressBookDBService == null)
+			addressBookDBService=new AddressBookDBService();
+		return addressBookDBService;
+	}
 
 	public List<ContactPerson> readData() {
 		String sql = "select * from contact_detail;";
@@ -18,18 +27,7 @@ public class AddressBookDBService {
 		try(Connection connection = this.getConnection()) {
 			Statement statement = connection.createStatement();
 			ResultSet result = statement.executeQuery(sql);
-			while(result.next()) {
-				String firstName = result.getString(1);
-				String lastName = result.getString(2);
-				String address=result.getString(3);
-				String state=result.getString(4);
-				String city=result.getString(5);
-				long zip=result.getInt(6);
-				long phoneNumber = result.getLong(7);
-				String email = result.getString(8);
-				contactList.add(new ContactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email));
-
-			}
+			contactList=this.getContactData(result);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -45,5 +43,68 @@ public class AddressBookDBService {
 		connection=DriverManager.getConnection(jdbcURL,userName,password);
 		System.out.println("Connection is successfull"+ connection);
 		return connection;
+	}
+
+
+	public int updateContactData(String firstName, String email) {
+			return this.updateContactDataUsingStatement(firstName,email);
+	}
+
+	private int updateContactDataUsingStatement(String firstName, String email) {
+		String sql = String.format("update contact_detail set email = '%s' where firstName = '%s';",email,firstName);
+		try(Connection connection = this.getConnection()) {
+			Statement statement = connection.createStatement();
+			int result = statement.executeUpdate(sql);
+			return result;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		} 
+		return 0;
+
+	}
+
+	public List<ContactPerson> getContactData(String firstName) {
+		List<ContactPerson> contactList = null;
+		if (this.addressBookDataStatement == null)
+			this.prepareStatementForContactData();
+		try {
+			addressBookDataStatement.setString(1, firstName);
+			ResultSet resultSet = addressBookDataStatement.executeQuery();
+			contactList = this.getContactData(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;	
+	}
+	
+	private void prepareStatementForContactData() {
+		try {
+			Connection connection = this.getConnection();
+			String sql=	"SELECT	* FROM contact_detail WHERE firstName = ?";
+			addressBookDataStatement = connection.prepareStatement(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private List<ContactPerson> getContactData(ResultSet result) {
+		List<ContactPerson> contactList = new ArrayList<>();
+		try{
+			while(result.next()) {
+				String firstName = result.getString(1);
+				String lastName = result.getString(2);
+				String address=result.getString(3);
+				String state=result.getString(4);
+				String city=result.getString(5);
+				long zip=result.getInt(6);
+				long phoneNumber = result.getLong(7);
+				String email = result.getString(8);
+				contactList.add(new ContactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
 	}
 }
