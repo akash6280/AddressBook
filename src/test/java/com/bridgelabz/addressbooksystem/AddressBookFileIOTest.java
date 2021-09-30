9e2d8c21dc86e407adb78eed11075f74feab2808
Akash Singh <akashsingh6280@gmail.com>
2021-10-01 04:17:30 +0530

    ADDED: test case to read from text file and verify  it

---
+++ src/test/java/com/bridgelabz/addressbooksystem/AddressBookFileIOTest.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.addressbooksystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.google.gson.Gson;;
+
+public class AddressBookFileIOTest {
+	
+
+	private AddressBookFacilitiesImpl addressbookservice;
+	private List<ContactPerson> contactList;
+	private ContactPerson contact1;
+	private ContactPerson contact2;
+	private long count;
+	@Before
+	public void initialise() {
+		contact1 = new ContactPerson("agam","singh","i life","bangalore","karnataka",560100,876478736,"abc@gmail.com");
+		contact2 = new ContactPerson("anubhav","singh","heritage villa","bangalore","karnataka",562125,987654212,"qwerty@gmail.com");
+		contactList=new ArrayList<>();
+		contactList.add(contact1);
+		contactList.add(contact2);
+		addressbookservice=new AddressBookFacilitiesImpl();
+		addressbookservice.addressList.add(new AddressBook("BOOK1",contactList));
+		count=0;	
+	}
+
+	@Test
+	public void whenDataReadFromTextFile_ShouldMatchCount() {	
+		AddressBookFileService addressbookservice=new AddressBookFileService();
+		List<String> contactList=addressbookservice.readDataFromTextFile();
+		Assert.assertEquals(2,contactList.size());
+	}
+	
+	
+
+}
