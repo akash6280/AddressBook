5e258bb03c7d738c8041d820f09aff7a5c500820
Akash Singh <akashsingh6280@gmail.com>
2021-10-02 02:01:31 +0530

    MODIFIED: test cases according to  ER diagram

--- src/test/java/com/bridgelabz/addressbooksystem/AddressBookDBIOTest.java
+++ src/test/java/com/bridgelabz/addressbooksystem/AddressBookDBIOTest.java
@@ -4,38 +4,37 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.ToLongFunction;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import com.bridgelabz.addressbooksystem.AddressBookFacilitiesImpl.IOService;
-
 public class AddressBookDBIOTest {	
 	@Test
 	public void givenContactInDB_WhenRetrieved_ShouldMatchContactCount(){
 		AddressBookFacilitiesImpl addressBookService=new AddressBookFacilitiesImpl();
-		List<ContactPerson> contactList =addressBookService.readContactData(IOService.DB_IO);
+		List<Contact> contactList =addressBookService.readContactData(IOEnum.DB_IO);
 		Assert.assertEquals(6, contactList.size());
 	}
 	
 	@Test
-	public void givenNewPhoneNumberForContact_WhenUpdated_ShouldSyncWithDB() {
+	public void givenNewEmailForContact_WhenUpdated_ShouldSyncWithDB() {
 		AddressBookFacilitiesImpl addressBookService=new AddressBookFacilitiesImpl();
-		List<ContactPerson> contactList = addressBookService.readContactData(IOService.DB_IO);
-		addressBookService.updateLastName("yash","yash@gmail.com");
-		boolean result = addressBookService.checkContactInSyncWithDB("yash");
+		List<Contact> contactList = addressBookService.readContactData(IOEnum.DB_IO);
+		addressBookService.updateEmail(897876374,"sisodia@gmail.com");
+		boolean result = addressBookService.checkContactInSyncWithDB(897876374);
 		Assert.assertTrue(result);
 	}
 	
 	@Test
 	public void givenDateRange_WhenQueried_ShouldReturnContactCount(){
 		AddressBookFacilitiesImpl addressBookService=new AddressBookFacilitiesImpl();
-		addressBookService.readContactData(IOService.DB_IO);
+		addressBookService.readContactData(IOEnum.DB_IO);
 		LocalDate startDate = LocalDate.of(2019,01,01);
 		LocalDate endDate = LocalDate.now(); 
-		List<ContactPerson> contactList = addressBookService.getContactsInADateRange(startDate,endDate);
+		List<Contact> contactList = addressBookService.getContactsInADateRange(startDate,endDate);
 		Assert.assertEquals(6, contactList.size());
 	}
 }
